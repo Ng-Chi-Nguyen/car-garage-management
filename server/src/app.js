8b595212d48fe3b96ee9ab261309967e40efb335
Ng-Chi-Nguyen <155863412+Ng-Chi-Nguyen@users.noreply.github.com>
2026-03-19 16:32:46 +0700

    chore: Tải các thư viên ở server và client, kết nối server với database

--- server/src/app.js
+++ server/src/app.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const errorHandler = require('./middleware/errorHandler');
-const routes = require('./routes');
+import express from "express";
+import dotenv from 'dotenv';
+import cors from "cors";
+import { connectDB } from "./db/prisma.js";
+
+dotenv.config();
 
 const app = express();
+const port_server = process.env.APP_PORT_SEVER;
+const port_client = process.env.APP_PORT_CLIENT;
 
 // Middleware
-app.use(helmet());
-app.use(cors());
-app.use(morgan('combined'));
+app.use(cors({
+  origin: `http://localhost:${port_client}`, // Cho phép React của bạn
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true // Quan trọng để gửi token/cookie sau này
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
-app.use('/', routes);
-
-// 404 handler
 app.use((req, res) => {
   res.status(404).json({
     success: false,
@@ -28,7 +29,8 @@ app.use((req, res) => {
   });
 });
 
-// Error handler (must be last)
-app.use(errorHandler);
+connectDB()
 
-module.exports = app;
+app.listen(port_server, () => {
+  console.log(`Server đang chạy tại: http://localhost:${port_server}`)
+})
