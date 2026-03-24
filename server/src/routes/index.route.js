1189240c16afbad62799cf8149cad910f63ff5d0
Ng-Chi-Nguyen <155863412+Ng-Chi-Nguyen@users.noreply.github.com>
2026-03-24 17:18:51 +0700

    feat: comment lại route.index cần đăng nhập để test ở postman

--- server/src/routes/index.route.js
+++ server/src/routes/index.route.js
@@ -14,23 +14,41 @@ import vehicleRoute from "./management/vehicle.route.js";
 
 const Routes = (app) => {
   const apiPrefixV1 = "/api/v1";
+
+  // login true
   const requireManagementAccess = [
     authMiddleware.requireAuth,
     authMiddleware.requireRoles(["Admin", "NhanVien"]),
   ];
 
+  // app.use(`${apiPrefixV1}/auth`, authRoute);
+  // app.use(`${apiPrefixV1}/customers`, ...requireManagementAccess, customerRoute); // KHACH_HANG
+  // app.use(`${apiPrefixV1}/car-brands`, ...requireManagementAccess, carBrandRoute); // HIEU_XE
+  // app.use(`${apiPrefixV1}/vehicles`, ...requireManagementAccess, vehicleRoute); // XE
+  // app.use(`${apiPrefixV1}/repair-orders`, ...requireManagementAccess, repairOrderRoute); // PHIEU_SUA_CHUA
+  // app.use(`${apiPrefixV1}/labor-fees`, ...requireManagementAccess, laborFeeRoute); // TIEN_CONG
+  // app.use(`${apiPrefixV1}/parts`, ...requireManagementAccess, partRoute); // VAT_TU
+  // app.use(`${apiPrefixV1}/repair-order-details`, ...requireManagementAccess, repairOrderDetailRoute); // CT_PHIEU_SUA_CHUA
+  // app.use(`${apiPrefixV1}/suppliers`, ...requireManagementAccess, supplierRoute); // NHA_CUNG_CAP
+  // app.use(`${apiPrefixV1}/stock-receipts`, ...requireManagementAccess, stockReceiptRoute); // PHIEU_NHAP_KHO
+  // app.use(`${apiPrefixV1}/stock-receipt-details`, ...requireManagementAccess, stockReceiptDetailRoute); // CT_PHIEU_NHAP
+  // app.use(`${apiPrefixV1}/payment-receipts`, ...requireManagementAccess, paymentReceiptRoute); // PHIEU_THU_TIEN
+
+
+  // Login false
+
   app.use(`${apiPrefixV1}/auth`, authRoute);
-  app.use(`${apiPrefixV1}/customers`, ...requireManagementAccess, customerRoute); // KHACH_HANG
-  app.use(`${apiPrefixV1}/car-brands`, ...requireManagementAccess, carBrandRoute); // HIEU_XE
-  app.use(`${apiPrefixV1}/vehicles`, ...requireManagementAccess, vehicleRoute); // XE
-  app.use(`${apiPrefixV1}/repair-orders`, ...requireManagementAccess, repairOrderRoute); // PHIEU_SUA_CHUA
-  app.use(`${apiPrefixV1}/labor-fees`, ...requireManagementAccess, laborFeeRoute); // TIEN_CONG
-  app.use(`${apiPrefixV1}/parts`, ...requireManagementAccess, partRoute); // VAT_TU
-  app.use(`${apiPrefixV1}/repair-order-details`, ...requireManagementAccess, repairOrderDetailRoute); // CT_PHIEU_SUA_CHUA
-  app.use(`${apiPrefixV1}/suppliers`, ...requireManagementAccess, supplierRoute); // NHA_CUNG_CAP
-  app.use(`${apiPrefixV1}/stock-receipts`, ...requireManagementAccess, stockReceiptRoute); // PHIEU_NHAP_KHO
-  app.use(`${apiPrefixV1}/stock-receipt-details`, ...requireManagementAccess, stockReceiptDetailRoute); // CT_PHIEU_NHAP
-  app.use(`${apiPrefixV1}/payment-receipts`, ...requireManagementAccess, paymentReceiptRoute); // PHIEU_THU_TIEN
+  app.use(`${apiPrefixV1}/customers`, customerRoute); // KHACH_HANG
+  app.use(`${apiPrefixV1}/car-brands`, carBrandRoute); // HIEU_XE
+  app.use(`${apiPrefixV1}/vehicles`, vehicleRoute); // XE
+  app.use(`${apiPrefixV1}/repair-orders`, repairOrderRoute); // PHIEU_SUA_CHUA
+  app.use(`${apiPrefixV1}/labor-fees`, laborFeeRoute); // TIEN_CONG
+  app.use(`${apiPrefixV1}/parts`, partRoute); // VAT_TU
+  app.use(`${apiPrefixV1}/repair-order-details`, repairOrderDetailRoute); // CT_PHIEU_SUA_CHUA
+  app.use(`${apiPrefixV1}/suppliers`, supplierRoute); // NHA_CUNG_CAP
+  app.use(`${apiPrefixV1}/stock-receipts`, stockReceiptRoute); // PHIEU_NHAP_KHO
+  app.use(`${apiPrefixV1}/stock-receipt-details`, stockReceiptDetailRoute); // CT_PHIEU_NHAP
+  app.use(`${apiPrefixV1}/payment-receipts`, paymentReceiptRoute); // PHIEU_THU_TIEN
 
 };
 
